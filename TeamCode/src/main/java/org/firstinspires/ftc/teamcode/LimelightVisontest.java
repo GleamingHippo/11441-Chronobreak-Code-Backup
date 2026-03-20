2ed437dd92ab97f8059e20618077683204a6efa4
Colton <Gleaminghippo@gmail.com>
2026-03-20 12:09:10 -0700

    Added telemetry data to check targeting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightVisontest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightVisontest.java
@@ -3,9 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
-
+@TeleOp
 public class LimelightVisontest extends LinearOpMode {
 
     private Limelight3A limelight;
@@ -13,12 +14,12 @@ public class LimelightVisontest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException
     {
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight = hardwareMap.get(Limelight3A.class, "Limelight");
 
         telemetry.setMsTransmissionInterval(11);
 
         limelight.pipelineSwitch(0);
-
+        waitForStart();
         /*
          * Starts polling for data.
          */
@@ -32,8 +33,12 @@ public class LimelightVisontest extends LinearOpMode {
                     telemetry.addData("tx", result.getTx());
                     telemetry.addData("ty", result.getTy());
                     telemetry.addData("Botpose", botpose.toString());
+                    telemetry.addData("Staus", "Target acquired");
                 }
+                else telemetry.addData("Staus", "No target");
             }
+            else telemetry.addData("Staus", "Null");
+            telemetry.update();
         }
     }
 }
